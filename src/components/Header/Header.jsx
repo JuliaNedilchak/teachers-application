53ae43629ad8ed67760a59b9f5ad8ee6960e002a
JuliaNedilchak <115113067+JuliaNedilchak@users.noreply.github.com>
2025-10-14 22:24:56 +0100

    header done without font

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import ukraine from "../../images/ukraine.svg";
 import css from "./Header.module.css";
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import login from "../../images/login.svg"
 
 const Header = () => {
   return (
-    <header>
+    <header className={css.header} >
     <div className={css.logo}>
         <img src={ukraine} alt="ukraine" width={28} height={28}/>
         <p>LearnLingo</p>
         </div>
-        <nav>
-            <NavLink to="/" end >Home</NavLink> 
-            <NavLink to= '/teachers'>Teachers</NavLink>
+        <nav className={css.navig}>
+            <NavLink className={css.nav} to="/" end >Home</NavLink> 
+            <NavLink  className={css.nav} to= '/teachers'>Teachers</NavLink>
         </nav>
-        <div>
+        <div className={css.auth}>
             <img src={login} alt="login sign"/>
+            <div className={css.links}>
+            <Link className={css.authorize} to="/login">Log in</Link>
+            <Link  className={css.authorize}  to="/registration">Registration</Link>
+            </div>
         </div>
     </header>
   )
