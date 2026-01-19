48833ffc6842795dcc713387116b65b54703cdc8
JuliaNedilchak <115113067+JuliaNedilchak@users.noreply.github.com>
2026-01-19 17:30:31 +0000

    css info

--- src/components/Teachers/TeacherCard/TeacherCard.jsx
+++ src/components/Teachers/TeacherCard/TeacherCard.jsx
@@ -4,25 +4,34 @@ const TeacherCard = ({teacher}) => {
   const {avatar_url,name,surname,languages,lesson_info,conditions, levels,rating, price_per_hour,lessons_done}=teacher;
   return (
     <article className={css.cardContainer}>
-    <div></div>
-      <div className={css.avatarWrapper}><img className={css.photo} src={avatar_url} alt='photo' width="96" height="96"></img></div>
+    <div className={css.infoPart}>
+      <div className={css.justPhoto}><div className={css.avatarWrapper}><img className={css.photo} src={avatar_url} alt='photo' width="96" height="96"></img></div>
+      </div>
+      <div className={css.noPhoto}>
       <div className={css.info}>
+        <div className={css.names}>
         <p>languages</p>
+        <p>{name} {surname}</p>
+        </div>
         <p>lessons online</p>
         <p>Lessons done: {lessons_done} </p>
         <p>rating: {rating}</p>
         <p>price / 1 hour: {price_per_hour}</p>
       </div>
-      <div>
-      <p>{name} {surname}</p>
+      <div className={css.numbers}>
+      
         <p>speaks: {languages}</p>
         <p>lessons info:{lesson_info} </p>
         <p>conditions: {conditions}</p>
       </div>
-      <button type='button'>load more</button>
-      <div>
-        <p>{levels}</p>
+      <button className={css.button} type='button'>load more</button>
+      <div className={css.levelSection}>
+        <p className={css.levels}>{levels}</p>
       </div>
+      </div>
+      </div>
+      
+      
     </article>
    
   )
