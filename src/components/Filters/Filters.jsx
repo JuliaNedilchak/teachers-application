186ee002f00fbe97803532f5fe8af850d8888262
JuliaNedilchak <115113067+JuliaNedilchak@users.noreply.github.com>
2026-01-28 21:30:04 +0000

    filters

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -9,24 +9,24 @@ const Filters = ({filters, onChange}) => {
   return (
     <div> <label htmlFor="languages"> languages</label>
       <select name='languages' value={filters.languages} id='languages' onChange={handleChange}>
-     
+     <option value="all">all</option>
       <option value="French">French</option>
        <option value="English">English</option>
         <option value="German">German</option>
 </select>
  <label htmlFor="levels"> levels of knowledge</label>
       <select name='levels' value={filters.levels} id='levels'  onChange={handleChange}>
-     
+     <option value="all">all</option>
       <option value="A1 Beginner">A1 Beginner</option>
        <option value="A2 Elementary">A2 Elementary</option>
         <option value="B1 Intermediate">B1 Intermediate</option>
 </select>
  <label htmlFor="price"> Price</label>
       <select name='price'  value={filters.price} id='price'  onChange={handleChange}>
-     
-      <option value="20 $">20 $</option>
-       <option value="30 $">30 $</option>
-        <option value="40 $">40 $</option>
+     <option value="all">all</option>
+      <option value="20">20 $</option>
+       <option value="30">30 $</option>
+        <option value="40">40 $</option>
 </select>
     </div>
   )
--- src/components/Teachers/TeacherList/TeachersList.jsx
+++ src/components/Teachers/TeacherList/TeachersList.jsx
@@ -9,10 +9,28 @@ const TeachersList = ({filters}) => {
         state.teachers.items);
         console.log('techers from store:', teachers )
         console.log(teachers[2]?.id )
+        console.log(teachers[3]?.price_per_hour, typeof teachers[3]?.price_per_hour)
     const status = useSelector(state=>state.teachers.status);
     const error = useSelector(state=> state.teachers.error);
     const STEP=3;
     const [visibleCount, setVisibleCount]=useState(STEP);
+const filteredTeachers= teachers.filter((t)=>{
+    const byLanguage=
+    filters.languages==='all' || 
+    (t.languages || []).includes(filters.languages)
+
+    const byLevel = filters.levels==='all' || 
+    (t.levels|| []).includes(filters.levels);
+
+    const byPrice=
+    filters.price==='all' || 
+    Number(t.price_per_hour)<=Number(filters.price);
+
+    return byLanguage && byLevel && byPrice;
+})
+useEffect(()=>{
+    setVisibleCount(STEP);}, [filters.languages, filters.level, filters.price]
+);
 
     useEffect(()=>{
         if(status==='idle'){
@@ -20,14 +38,18 @@ const TeachersList = ({filters}) => {
         }
     }, [status,dispatch]);
 
+    
+
     if(status==='loading'){
         return <p>loading....</p>
     }
 if(status==='failed'){
     return <p>Error: {error}</p>
 }
-const visibleTeachers=teachers.slice(0,visibleCount);
-const canLoadMore=visibleCount<teachers.length;
+
+
+const visibleTeachers=filteredTeachers.slice(0,visibleCount);
+const canLoadMore=visibleCount<filteredTeachers.length;
   return (
     <div>
      {visibleTeachers.map((teacher)=>(
