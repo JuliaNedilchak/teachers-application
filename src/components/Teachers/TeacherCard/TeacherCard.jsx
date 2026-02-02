289a0a6ebc6c179363e117e325687f5a9fd2a5b4
JuliaNedilchak <115113067+JuliaNedilchak@users.noreply.github.com>
2026-02-02 13:23:18 +0000

    css card

--- src/components/Teachers/TeacherCard/TeacherCard.jsx
+++ src/components/Teachers/TeacherCard/TeacherCard.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import css from "./TeacherCard.module.css";
 import { Link } from 'react-router-dom';
+import book from '../../../images/book.svg'
+import star from '../../../images/star.svg'
+import heart from '../../../images/heart.svg'
+
+
 const TeacherCard = ({teacher}) => {
   const {avatar_url,name,surname,languages,lesson_info,conditions, levels,rating, price_per_hour,lessons_done}=teacher;
   return (
@@ -11,14 +16,21 @@ const TeacherCard = ({teacher}) => {
       <div className={css.noPhoto}>
       <div className={css.info}>
         <div className={css.names}>
-        <p>languages</p>
-        <p>{name} {surname}</p>
+        <p className={css.lang}>languages</p>
+       </div>
+        <div className={css.stats}>
+        <div className={css.lessons_online}>
+        <img src={book} alt='lessons book' width={16} height={16}/>
+        <span className={css. online}>lessons online</span> | </div> 
+        <div className={css.done_container}> <span className={css.lessons_done}>Lessons done: {lessons_done} </span> |</div>
+        <div className={css.rating}>
+        <img src={star} alt='star' width={16} height={16}/><span className={css.ratingText}>rating: {rating}</span> |</div>
+        <span className={css.priceText}>price / 1 hour: {price_per_hour}</span>
+        <div className={css.heartFrame}><img className={css.heart} src={heart} alt='heart' width={26} height={26}/></div>
         </div>
-        <p>lessons online</p>
-        <p>Lessons done: {lessons_done} </p>
-        <p>rating: {rating}</p>
-        <p>price / 1 hour: {price_per_hour}</p>
+         
       </div>
+       <p>{name} {surname}</p>
       <div className={css.numbers}>
       
         <p>speaks: {languages}</p>
