6d31636adf4b23951df4f781d2a02045ce64e5b6
JuliaNedilchak <115113067+JuliaNedilchak@users.noreply.github.com>
2026-02-05 18:14:15 +0000

    read more part

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@
 import HomePage from './pages/HomePage/HomePage';
 import { Route, Routes } from 'react-router-dom';
 import Teachers from './pages/TeachersPage/Teachers';
-import TeachersDetailsPage from './pages/TeachersDetailsPage/TeachersDetailsPage';
+
  
  const App = () => {
    return (
     <Routes>
       <Route path="/" element={<HomePage/>}/>
       <Route path="/teachers" element={<Teachers/>}/>
-       <Route path="/teachers/:id" element={<TeachersDetailsPage/>}/>
+       
     </Routes>
    )
  }
--- src/components/Teachers/TeacherCard/TeacherCard.jsx
+++ src/components/Teachers/TeacherCard/TeacherCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import css from "./TeacherCard.module.css";
 import { Link } from 'react-router-dom';
 import book from '../../../images/book.svg'
@@ -8,6 +8,7 @@ import heart from '../../../images/heart.svg'
 
 const TeacherCard = ({teacher}) => {
   const {avatar_url,name,surname,languages,lesson_info,conditions, levels,rating, price_per_hour,lessons_done}=teacher;
+  const [isOpen, setIsOpen]=useState(false);
   return (
     <article className={css.cardContainer}>
     <div className={css.infoPart}>
@@ -15,10 +16,11 @@ const TeacherCard = ({teacher}) => {
       </div>
       <div className={css.noPhoto}>
       <div className={css.info}>
+        
+        <div className={css.stats}>
         <div className={css.names}>
         <p className={css.lang}>languages</p>
        </div>
-        <div className={css.stats}>
         <div className={css.lessons_online}>
         <img src={book} alt='lessons book' width={16} height={16}/>
         <span className={css. online}>lessons online</span> | </div> 
@@ -28,20 +30,39 @@ const TeacherCard = ({teacher}) => {
         <span className={css.priceText}>price / 1 hour: {price_per_hour}</span>
         <div className={css.heartFrame}><img className={css.heart} src={heart} alt='heart' width={26} height={26}/></div>
         </div>
-         
+        <div className={css.nameCont}>
+          <p className={css.nameTeacher}>{name} {surname}</p></div>
       </div>
-       <p>{name} {surname}</p>
+      
       <div className={css.numbers}>
       
-        <p>speaks: {languages}</p>
-        <p>lessons info:{lesson_info} </p>
-        <p>conditions: {conditions}</p>
+        <p className={css.start}>speaks: <span className={css.startText}>{languages.join(',')}</span></p>
+        <p className={css.start}>lessons info: <span className={css.startText}>{lesson_info}</span> </p>
+        <p className={css.start}>conditions: <span className={css.startText}> {conditions}</span></p>
       </div>
-      <Link to={`/teachers/${teacher.id}`}>Read more</Link>
-      <div className={css.levelSection}>
-        <p className={css.levels}>{levels}</p>
+       {isOpen && 
+       (<div>
+       <p className={css.experience}>{teacher.experience} </p>
+       <ul>
+       <li>
+        <div>
+          <p>{teacher.reviews.map(review=> (
+           <div><p>{review.reviewer_name}</p>
+           <div><img src={star} alt='star' width={16} height={16}/><p>{review.reviewer_rating}</p></div></div>
+          
+          ))}</p>
+        </div>
+       </li>
+</ul>
+       </div>)}
+      <button type='button' onClick={()=> setIsOpen(prev=> !prev)}>{isOpen? 'Read less' : 'Read more'}</button>
+     
+        <div className={css.levelSection}>{levels.map(level=>(
+          <span className={css.levelItem} key={level}>{level}</span>
+        ))}</div>
+      
       </div>
-      </div>
+      
       </div>
       
       
